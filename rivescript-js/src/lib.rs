f26f2ab22578554e4488f66bb933f65de96688d9
Noah Petherbridge <root@kirsle.net>
2026-04-29 10:12:55 -0700

    WIP: JavaScript engine with boa
    * The JavaScript handler for object macros basically works now and can
      execute basic JavaScript code.
    * To Do: the RiveScript proxy object passed in to JS is not yet
      implemented, the object macro instead receives a null object
      prototype.

--- rivescript-js/src/lib.rs
+++ rivescript-js/src/lib.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use async_trait::async_trait;
+use boa_engine::{Context, JsObject, JsResult, JsString, JsValue, Source, context::intrinsics::Intrinsics, native_function::NativeFunction};
 
 use rivescript_core::macros::{LanguageLoader, Proxy, SubroutineResult};
 
@@ -21,16 +22,74 @@ impl LanguageLoader for JavaScriptLoader {
         self.sources.insert(name.to_string(), code.join("\n"));
         Ok(true)
     }
-    async fn call(&self, _proxy: &dyn Proxy, name: &str, _args: Vec<String>) -> Result<SubroutineResult, String> {
-        match self.sources.get(name) {
-            Some(code) => return Ok(SubroutineResult{
-                    output: code.to_string(),
-                    staged_bot_vars: HashMap::new(),
-                    staged_user_vars: HashMap::new(),
-            }),
-            None => {
-                return Err(format!("[ERR: Object '{}' Not Found]", name));
-            }
+
+    async fn call(&self, proxy: &dyn Proxy, name: &str, args: Vec<String>) -> Result<SubroutineResult, String> {
+        let code = self.sources.get(name)
+            .ok_or_else(|| format!("[ERR: Object '{}' Not Found]", name))?;
+
+        // Initialize Boa context.
+        let mut context = Context::default();
+        let global = context.global_object();
+
+        // Set up the RiveScript "bridge" object.
+        let proxy_mutex = std::sync::Arc::new(tokio::sync::Mutex::new(proxy));
+        let rs_obj = JsObject::with_null_proto();
+
+        // Define the rs.currentUser() -> proxy.current_username() bridge.
+        // let p_clone = proxy_mutex.clone();
+        // let current_user_fn = NativeFunction::from_copy_closure(move |_this, _args, _context| {
+        //     let mut p = p_clone.try_lock()
+        //         .map_err(|_| boa_engine::JsNativeError::error().with_message("Proxy lock contention"))?;
+        //     let name = p.current_username().unwrap_or_else(|_| rivescript_core::UNDEFINED.to_string());
+        //     Ok(JsValue::from(JsString::from(name)))
+        // });
+
+        // rs_obj.set(
+        //         JsString::from("currentUser"),
+        //         context.create_native_function(
+        //             JsString::from("currentUser"),
+        //             current_user_fn,
+        //         ),
+        //         false,
+        //         &mut context
+        //     )
+        //     .map_err(|e| format!("JS Setup Error: {}", e))?;
+
+        // Turn it into a JavaScript function and evaluate it.
+        let js_src = format!("function objectMacro (rs, args) {{ {} }}", code);
+        println!("EVAL: {}", js_src);
+        context.eval(Source::from_bytes(js_src.as_bytes()))
+            .map_err(|e| format!("JS Eval Error: {}", e))?;
+
+        // Prepare arguments for the JS function call.
+        let js_args = boa_engine::object::builtins::JsArray::from_iter(
+            args.iter().map(|s| JsValue::from(JsString::from(s.as_str()))),
+            &mut context,
+        );
+        let dispatch_args = [
+            rs_obj.into(),
+            js_args.into(),
+        ];
+        println!("JS args: {:#?}", dispatch_args);
+
+        // Call the function.
+        let func = global.get(JsString::from("objectMacro"), &mut context)
+            .map_err(|_| format!("JS Function '{}' not defined in object", name))?;
+
+        if let Some(callable) = func.as_callable() {
+            let result = callable.call(&JsValue::undefined(), &dispatch_args, &mut context)
+                .map_err(|e| format!("JS Execution Error: {}", e))?;
+
+            // Return the output.
+            Ok(SubroutineResult{
+                output: result.to_string(&mut context)
+                    .map(|js_str| js_str.to_std_string_escaped())
+                    .unwrap_or_default(),
+                staged_bot_vars: HashMap::new(),
+                staged_user_vars: HashMap::new(),
+            })
+        } else {
+            Err(format!("Object '{}' is not a callable function in JS", name))
         }
     }
 }
